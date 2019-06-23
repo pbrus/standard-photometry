ae0a344bd4096491cea7875c79ea2557d7b31ab5
Przemysław Bruś <brus@astro.uni.wroc.pl>
2019-06-23 23:09:49 +0200

    Add docstrings to the Columns class

--- sphot/data.py
+++ sphot/data.py
@@ -6,10 +6,38 @@ class Columns:
     """Handle an argument of columns list."""
 
     def __init__(self, columns):
+        """
+        Set a list of columns.
+
+        Parameters
+        ----------
+        columns : list
+            A list of columns. Each column should be represented
+            by a string. The string must contain an integer or a range
+            of integers. For example ['2', '4', '7:11', '15'].
+        """
         self.columns = columns
         self.indexes = self.indexes(columns)
 
     def indexes(self, columns):
+        """
+        Transform a list of columns to another form. For example:
+        ['2', '4', '7:11', '15'] -> [1, 3, 6, 7, 8, 9, 10, 14]
+
+        Parameters
+        ----------
+        columns : list
+            A list of columns. Each column should be represented
+            by a string. The string must contain an integer or a range
+            of integers separated by a colon.
+
+        Returns
+        -------
+        indexes : list
+            A list of columns. Each column is represented by a single
+            integer and the numbers of columns are transformed into indexes
+            (shifting the numbers by -1).
+        """
         indexes = []
 
         for column in self.columns:
@@ -25,7 +53,21 @@ class Columns:
         return indexes
 
     def column_type(self, column):
+        """
+        Check a type of a column. It could be a single integer or a range
+        of integers. Raise an exception if the type is improper.
 
+        Parameters
+        ----------
+        column : str
+            A string which contains an integer or a range of integers.
+
+        Returns
+        -------
+        str
+            A string which represents the type of the column. Possible
+            values are: 'number' or 'range'.
+        """
         if search('^[0-9]{1,}$', column):
             return "number"
         elif search('^[0-9]{1,}:[0-9]{1,}$', column):
@@ -35,10 +77,28 @@ class Columns:
 
     @staticmethod
     def split_range(column):
+        """
+        Get minimum and maximum values of a range.
+
+        Parameters
+        ----------
+        column : str
+            A string which represents a range as two values separated
+            by a colon.
+
+        Returns
+        -------
+        tuple
+            Minimum and maximum values of the range.
+        """
         start, end = map(int, column.split(":"))
         return start, end
 
     def validate_range(self, start, end):
+        """
+        Check whether a range defined by two values is increasing.
+        Raise an exception if not.
+        """
         if start >= end:
             column = str(start) + ":" + str(end)
             raise ValueError("Invalid column argument {}".format(column))
