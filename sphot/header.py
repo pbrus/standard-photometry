ff923574c47f44b7024b86cebe9168f4fd94a53a
Przemysław Bruś <brus@astro.uni.wroc.pl>
2019-06-30 18:52:19 +0200

    Update docstrings in the Header class

--- sphot/header.py
+++ sphot/header.py
@@ -1,13 +1,13 @@
+"""
+The Header class transforms a list of columns to the list of labels.
+"""
 import pandas as pd
 from itertools import product
 from .columns import Columns
 
 
 class Header(Columns):
-    """
-    Handle a header of a text file. Transform
-    a list of columns to the list of labels.
-    """
+    """Handle a header of a text file."""
 
     __suffixes = [
         "_ins",
@@ -38,6 +38,7 @@ class Header(Columns):
         self._get_passband_column_labels()
 
     def _check_column_numbers(self):
+        """Check whether the number of column is proper."""
         n = len(self.__suffixes)
         total, rest = divmod(len(self.indexes), n)
 
@@ -46,12 +47,14 @@ class Header(Columns):
                 "Number of columns must be equal ({}n + 1).".format(n))
 
     def _read_header(self):
+        """Read a header file."""
         self.__header = pd.read_csv(
             self.filename, usecols=self.indexes, sep="\s+", nrows=0)
 
         return self.__header
 
     def _get_passband_labels(self):
+        """Get unique passband labels from a file's header."""
         suff = self.__suffixes[0]
         self.__passband_labels = [
             name[:-len(suff)] for name in
@@ -61,6 +64,7 @@ class Header(Columns):
         return self.__passband_labels
 
     def _get_passband_column_labels(self):
+        """Get all labels which represent passbands columns."""
         self.__column_labels = []
 
         for column_name in product(self.__passband_labels, self.__suffixes):
@@ -78,6 +82,7 @@ class Header(Columns):
         return self.__column_labels
 
     def _get_identifier_column_label(self, passband_column_names):
+        """Get a label which represent a column with identifiers."""
         self.__identifier_label = (
             set(self.__header.columns) - set(passband_column_names))
         self.__identifier_label = str(*self.__identifier_label)
