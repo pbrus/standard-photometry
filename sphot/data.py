f551e0cd3289751a00b36e541601d59b20157489
Przemysław Bruś <brus@astro.uni.wroc.pl>
2019-07-12 19:08:37 +0200

    Add docstrings to the Data class

--- sphot/data.py
+++ sphot/data.py
@@ -1,3 +1,8 @@
+"""
+The Data class reads columns from a text file. Each column is represented
+by brightness in a specific passband. The class calculates differences
+of consecutive passbands (colors) and filters such data.
+"""
 import numpy as np
 import pandas as pd
 
@@ -9,6 +14,21 @@ class Data(Header):
     __min_error = 0.0001
 
     def __init__(self, filename, columns, null_marker=99.9999):
+        """
+        Set parameters to get data from a text file.
+
+        Parameters
+        ----------
+        filename : str
+            A name of the input text file.
+        columns : list
+            A list of columns. Each column should be represented
+            by a string. The string must contain an integer or a range
+            of integers. For example ['2', '4', '7:11', '15'].
+        null_marker : float, int, NaN
+            A value indicating that there is no measurement
+            in the input file.
+        """
         super().__init__(filename, columns)
         self.filename = filename
         self.__null_marker = null_marker
@@ -18,32 +38,38 @@ class Data(Header):
         self._calculate_sets()
 
     def _read_data(self):
+        """Read data from a file."""
         self.__dataframe = pd.read_csv(
             self.filename, sep=r'\s+', usecols=self.labels)
 
         return self.__dataframe
 
     def _handle_nulls(self):
+        """Replace null markers by default NaN value."""
         self.__dataframe.mask(
             self.__dataframe == self.__null_marker, inplace=True)
 
     def _set_minimum_error(self):
+        """Set a minimum value for errors to prevent division by zero."""
         self.__dataframe.mask(self.__dataframe < self.__min_error,
                               self.__min_error, inplace=True)
 
     def _xy_labels(self, standard, instrumental):
+        """Generator for sets of column's labels."""
         for labels in zip(standard, instrumental, instrumental[1:]):
             yield labels
 
         yield standard[-1], instrumental[-2], instrumental[-1]
 
     def _column_name_generator(self, first_label, second_label):
+        """Wrapper for column labels generator."""
         first_column_labels = self._get_specific_labels(first_label)
         second_column_labels = self._get_specific_labels(second_label)
 
         return self._xy_labels(first_column_labels, second_column_labels)
 
     def _calculate_sets(self):
+        """Transform columns with passbands to proper sets."""
         self.__sets = []
         passbands_generator = self._column_name_generator("_std", "_ins")
         errors_generator = self._column_name_generator("_serr", "_ierr")
@@ -72,4 +98,21 @@ class Data(Header):
 
     @property
     def sets(self):
+        """
+        Get sets of data for each panel, i.e. differences of magnitudes
+        on the X and Y axes.
+
+        Returns
+        -------
+        list
+            A list which contains DataFrame objects. Each object
+            represents four columns:
+            - instrumental_magnitude_i - instrumental_magnitude_j
+            - errors of above difference
+            - standard_magnitude_i - instrumental_magnitude_i
+            - errors of above difference
+
+            i and j are indexes. These objects don't contain rows with
+            null_marker - see the __init__ method.
+        """
         return self.__sets
